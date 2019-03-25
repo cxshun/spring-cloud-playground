71fc48034192b829d7f5b514a19b4b59e38f8b6f
chenxs <chenxs@xiaopeng.com>
2019-03-25 13:58:23 +0800

    add simple server

---
+++ eureka-simple-server/src/main/java/com/shun/EurekaSimpleServer.java
@@ -0,0 +1,19 @@
+package com.shun;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+/**
+ * @author chenxs(chenxs @ xiaopeng.com)——车联网基础平台
+ * 2019-03-19 11:31
+ **/
+@SpringBootApplication
+@EnableEurekaServer
+public class EurekaSimpleServer {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EurekaSimpleServer.class, args);
+    }
+
+}
