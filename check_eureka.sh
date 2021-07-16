ff56cd2971aa772d1f83509502fa6b3e53fe546a
cxshun <cxshun@gmail.com>
2021-07-16 17:33:44 +0800

    add check_eureka and healthcheck

---
+++ check_eureka.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+while:
+  do
+    CODE = `curl -I -m 10 -o /dev/null -s -w %{http_code} http://eureka-server:8801/`
+    if [[ $CODE -eq 200 ]]; then
+      echo -e "\033[42;34m eureka-server is ok \033[0m"
+      break;
+    else
+      sleep 1
+    fi
+  done
+
+java -jar $1.jar
