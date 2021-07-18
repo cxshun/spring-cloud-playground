0893ffc92a398e2b619d1a9ad11471e27f1a57e9
cxshun <cxshun@gmail.com>
2021-07-18 21:13:07 +0800

    fix dockerfile

--- admin-server/check_eureka.sh
+++ admin-server/check_eureka.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
-while:
+while :
   do
-    CODE = `curl -I -m 10 -o /dev/null -s -w %{http_code} http://eureka-server:8801/`
+    CODE=`curl -I -m 10 -o /dev/null -s -w %{http_code} http://eureka-server:8801/`
     if [[ $CODE -eq 200 ]]; then
       echo -e "\033[42;34m eureka-server is ok \033[0m"
       break;
